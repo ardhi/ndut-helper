1266669b2145778fcba466f518930c06838bafc9
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-07 19:57:44 +0700

    Defaults to empty object

--- src/get-ndut-config.js
+++ src/get-ndut-config.js
@@ -2,5 +2,5 @@ const _ = require('lodash')
 
 module.exports = function (fastify, name, returnIndex = false) {
   if (returnIndex) return _.findIndex(fastify.config.nduts, { name })
-  return _.find(fastify.config.nduts, { name })
+  return _.find(fastify.config.nduts, { name }) || {}
 }
