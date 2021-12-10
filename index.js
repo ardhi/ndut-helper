d751931a0ff152a35924b10ad9c99bb1e5542336
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-10 12:38:02 +0700

    Add luxon

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const _ = require('lodash')
 const fastGlob = require('fast-glob')
+const luxon = require('luxon')
 const fs = require('fs-extra')
 const aneka = require('aneka')
 const fp = require('fastify-plugin')
 const scramjet = require('scramjet')
 const JSONStream = require('JSONStream')
 
-const mod = { _, fastGlob, fs, aneka, fp, scramjet, JSONStream }
+const mod = { _, fastGlob, fs, aneka, fp, scramjet, JSONStream, luxon }
 
 mod.mixPlugins = require('./mix-plugins')
 mod.scanForRoutes = require('./scan-for-routes')
